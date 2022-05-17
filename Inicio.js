2cd197986504f7349cb7ae68d0b63682411bdead
intarqta <105185644+intarqta@users.noreply.github.com>
2022-05-16 22:28:01 -0300

    Update Inicio.js

--- Inicio.js
+++ Inicio.js
@@ -39,3 +39,109 @@ botonCerrarMenu.addEventListener('click', function(){
     botonMenu.classList.remove('active');
     botonCerrarMenu.classList.remove('active');
 })
+
+// Gráficas mediante Chart.js
+let graStockGanadero = document.getElementById('gra-panel-gan').getContext("2d");
+
+// Función que permite extraer datos desde google sheets 
+
+fetch('https://sheets.googleapis.com/v4/spreadsheets/1lhEM-P2yW2MqMAaGeuUn9VVJbYK7fPhRKPnoOKkyvP8/values/Hoja 1!A1:AW?key=AIzaSyAmXGXaGj8Gy6a5HvlJAMGms1fsnciHlEI')
+    .then(res => res.json())
+    .then(datos1 => {
+        tabla(datos1)
+    })
+function tabla(datos1){
+    const datos = datos1.values.filter(data =>{
+        return data[1] ==='General Obligado';
+    })
+    const datosVera = datos1.values.filter(data =>{
+        return data[1] ==='Vera';
+    })
+/*Carga de gráficas y link de descarga dentro del popup Datos*/     
+const canvas = document.getElementById('gra-panel-gan');
+const canvas2 = document.getElementById('gra-panel-gan2');
+const ctx = canvas.getContext('2d');
+const ctx2 = canvas2.getContext('2d');
+
+
+const configChart = {
+  type: 'horizontalBar',
+  data: {
+     labels: datos.map(data =>{return data[0]}),
+     datasets: [{
+        label: 'Novillos ',
+        data: datos.map(data =>{return data[4]}),
+        borderColor:"rgba(236, 35, 35, 0.75)",
+        backgroundColor: 'rgba(236, 35, 35,0.75)'
+     },
+     {
+        label: 'Vacas ',
+        data: datos.map(data =>{return data[2]}),
+        borderColor:'rgba(35, 102, 236,0.75)',
+        backgroundColor: 'rgba(35, 102, 236,0.5)'
+     },
+     {
+        label: 'Toros ',
+        data: datos.map(data =>{return data[8]}),
+        borderColor:'rgb(35, 129, 20)',
+        backgroundColor: 'rgba(35, 129, 20,1)'
+     },
+     {
+        label: 'Vaquillonas ',
+        data: datos.map(data =>{return data[3]}),
+        borderColor:'rgba(81, 26, 132, 1)',
+        backgroundColor: 'rgba(81, 26, 132, 0.792)'
+     },
+     {
+        label: 'Novillitos ',
+        data: datos.map(data =>{return data[5]}),
+        borderColor:'rgba(58, 2, 25, 0.944)',
+        backgroundColor: 'rgba(58, 2, 25, 0.944)'
+     }  
+    ]
+  } 
+
+ }
+ const configChart2 = {
+    type: 'bar',
+    data: {
+       labels: datosVera.map(data =>{return data[0]}),
+       datasets: [{
+          label: 'Novillos ',
+          data: datosVera.map(data =>{return data[4]}),
+          borderColor:"rgba(236, 35, 35, 0.75)",
+          backgroundColor: 'rgba(236, 35, 35,0.75)'
+       },
+       {
+          label: 'Vacas ',
+          data: datosVera.map(data =>{return data[2]}),
+          borderColor:'rgba(35, 102, 236,0.75)',
+          backgroundColor: 'rgba(35, 102, 236,0.5)'
+       },
+       {
+          label: 'Toros ',
+          data: datosVera.map(data =>{return data[8]}),
+          borderColor:'rgb(35, 129, 20)',
+          backgroundColor: 'rgba(35, 129, 20,1)'
+       },
+       {
+          label: 'Vaquillonas ',
+          data: datosVera.map(data =>{return data[3]}),
+          borderColor:'rgba(81, 26, 132, 1)',
+          backgroundColor: 'rgba(81, 26, 132, 0.792)'
+       },
+       {
+          label: 'Novillitos ',
+          data: datosVera.map(data =>{return data[5]}),
+          borderColor:'rgba(58, 2, 25, 0.944)',
+          backgroundColor: 'rgba(58, 2, 25, 0.944)'
+       }  
+      ]
+    } 
+  
+   }
+ // añadir el gráfico y asignarle la clase active 
+ myChart =  new Chart(ctx, configChart);
+ // añadir el gráfico y asignarle la clase active 
+ myChart2 =  new Chart(ctx2, configChart2);
+}
