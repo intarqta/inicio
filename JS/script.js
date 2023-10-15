ab451652c9927a491657c7426c9959bccedcccd9
intarqta <105185644+intarqta@users.noreply.github.com>
2023-10-15 17:52:29 -0300

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// Función interectiva para el menu superior
+$(document).ready(function(){
+    var altura = $('.container-fluid').offset().top;
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() > altura){
+            $('.container-fluid').addClass('active');
+
+        }else{
+            $('.container-fluid').removeClass('active');
+        }
+    })
+})
+// Modificar el tamaño del footer al hacer scroll
+// Función interectiva para el menu superior
+$(document).ready(function(){
+    var altura = $('#footer').offset().top;
+  
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() <1000){ 
+            $('footer').addClass('active');
+            $('links').addClass('active');
+            $('Copyright').addClass('active');            
+
+        }else{
+            $('footer').removeClass('active');
+            $('links').removeClass('active');
+            $('Copyright').removeClass('active');     
+        }
+    })
+})
