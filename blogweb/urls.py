8285c1a807f764de423bbd6ed7334f9f5919bf7a
intarqta <105185644+intarqta@users.noreply.github.com>
2023-12-07 20:35:32 -0300

    Update urls.py

--- blogweb/urls.py
+++ blogweb/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name="index"),
-    path('nosotros/', views.nosotros, name="nosotros"),
+    path('', views.mapa, name="index"),
+    path('nosotros/', views.mapa, name="nosotros"),
     path('mapa/', views.mapa, name="mapa"),
-    path('menu/', views.menu, name="menu"),
-    path('novedades/', views.novedades, name="novedades"),
-    path('form/', views.form, name="form"),
-    path('postform/', views.okform, name="okform"),
+    path('menu/', views.mapa, name="menu"),
+    path('novedades/', views.mapa, name="novedades"),
+    path('form/', views.mapa, name="form"),
+    path('postform/', views.mapa, name="okform"),
 ]
