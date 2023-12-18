60bbb693b40c57338294e417ef99fa2b3c8ed595
intarqta <105185644+intarqta@users.noreply.github.com>
2023-12-18 09:38:44 -0300

    Add files via upload

---
+++ blogweb/static/JS/Departamentos_NS.js
@@ -0,0 +1,6 @@
+var Departamentos = {
+    "type": "FeatureCollection",
+    "name": "Departamentos_NS",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": []
+}
